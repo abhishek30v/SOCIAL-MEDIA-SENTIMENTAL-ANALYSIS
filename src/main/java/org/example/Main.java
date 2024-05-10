99dfe5bd3de6bafc1ca87109871e69efc5d3bf06
Abhishek Verma <116665292+abhishek30v@users.noreply.github.com>
2024-05-10 23:37:14 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.io.File;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        File dataset = new File("src/main/resources/Tweets.csv");
+        Processor processor = new Processor();
+        try {
+            File formattedDataset = processor.getPreProcessor().reFormatData(dataset);
+            processor.getAnalyzer().analyze(formattedDataset);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+}
+
+
+ 
