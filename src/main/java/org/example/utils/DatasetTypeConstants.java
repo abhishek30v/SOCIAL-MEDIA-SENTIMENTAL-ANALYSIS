cea3507626c21b08403a454d5abd4cefbb5a2c74
Abhishek Verma <av7257383@gmail.com>
2024-05-14 23:46:44 +0530

    ADDED DATASET TYPE CONSTANTS

---
+++ src/main/java/org/example/utils/DatasetTypeConstants.java
@@ -0,0 +1,12 @@
+package org.example.utils;
+
+public class DatasetTypeConstants {
+    public static final String RAW_CSV = "raw csv";
+    public static final String CLEAN_CSV = "clean csv";
+
+    public static final String TRAINING_DATA = "training data";
+
+    public static final String TESTING_DATA = "testing data";
+
+    public static final String TARGET_DATA = "target data";
+}
